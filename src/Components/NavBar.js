64be8502dfc1b35d43b0a4a87375bc00f0250b1d
Xaroz <jasonguo2013jg@gmail.com>
2019-11-07 11:57:11 -0400

    Added Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Avatar from "../Images/Avatar.png";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  const setMenu = () => {
+    const currentValue = displayMenu;
+    setDisplayMenu(!currentValue);
+  };
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header>
+      {/* <h1 className="logo">TBD</h1> */}
+      <div className="img-container" onClick={setMenu}>
+        <img src={Avatar} alt="user" />
+        {displayMenu ? (
+          <div className="menu">
+            <ul>
+              <hr />
+              <li>
+                <Link to="/">Cerrar Session</Link>
+              </li>
+              <hr />
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </header>
   );
 };
