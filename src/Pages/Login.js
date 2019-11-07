4442d6371908488cc4a593465b43f88157ddf4b5
Xaroz <jasonguo2013jg@gmail.com>
2019-11-07 14:30:50 -0400

    Validation in login and alert message

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import Input from "../Components/Input";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export const Login = () => {
   const initialFormState = {
     username: "",
     password: ""
   };
+  const fakeUser = {
+    username: "admin",
+    password: "123admin"
+  };
 
   const [login, setLogin] = useState(initialFormState);
+  const [wrongCredential, setWrongCredential] = useState(false);
+  let history = useHistory();
 
   const handleInputChange = event => {
     const { name, value } = event.target;
     setLogin({ ...login, [name]: value });
   };
+  const validateUser = event => {
+    event.preventDefault();
+    if (
+      login.username === fakeUser.username &&
+      login.password === fakeUser.password
+    ) {
+      history.push("/dashboard");
+    } else {
+      setLogin(initialFormState);
+      setWrongCredential(true);
+    }
+  };
   return (
     <div className="login-container">
       <h1>Iniciar sesión</h1>
       <div className="login">
-        <form className="login-form">
+        <form className="login-form" onSubmit={validateUser}>
           <Input
             name="username"
             text="Usuario"
@@ -30,12 +48,12 @@ export const Login = () => {
             typeName="password"
             handleInputChange={handleInputChange}
           />
-        </form>
-
-        <Link className="button" to="/dashboard">
           <button>Acceder</button>
-        </Link>
+        </form>
       </div>
+      {wrongCredential ? (
+        <h4 className="wrong-credential">Credenciales equivocadas</h4>
+      ) : null}
     </div>
   );
 };
